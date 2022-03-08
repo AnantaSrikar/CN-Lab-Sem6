e12a1ea9999022526ac88b6a50d7964f3cd78630
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-03-08 11:04:16 +0530

    lab_1: Solved task-1 using tcp

---
+++ lab_1/tcp/t1/client.py
@@ -0,0 +1,14 @@
+# Basic TCP client
+# Author: Srikar
+
+import socket
+
+HOST = "127.0.0.1"	# The server's hostname or IP address
+PORT = 65432 		# The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.connect((HOST, PORT))
+	server_data = s.recv(1024)
+	s.sendall(b"I can hear you!")
+
+print(f"Received: {server_data.decode()}")
---
+++ lab_1/tcp/t1/server.py
@@ -0,0 +1,34 @@
+# Basic TCP server
+# Author: Srikar
+
+import socket
+
+HOST = "127.0.0.1"	# Standard loopback interface address (localhost)
+PORT = 65432		# Port to listen on (non-privileged ports are > 1023)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	try:
+		s.bind((HOST, PORT))
+		s.listen()
+
+	except OSError:
+		print("Port is busy! You're probably running the server already?")
+		exit()
+
+	while True:
+		try:
+			conn, addr = s.accept()
+			with conn:
+				print(f"Connected by {addr}")
+				conn.sendall(b"Can you hear me?")
+				client_data = conn.recv(1024)
+				print(f"Recieved from Client: {client_data.decode()}")
+
+		except KeyboardInterrupt:
+			print('Shutting down the server...')
+			break
+
+		# We should ideally never hit this, but nobody can predict the future ¯\_(ツ)_/¯
+		except Exception as e:
+			print(f"Something bad happened: {e}")
