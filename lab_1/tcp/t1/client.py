f134ef299426e2777563ff0b23894388936a93f0
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-03-08 13:49:00 +0530

    Improved exception handling - Changed port number

--- lab_1/tcp/t1/client.py
+++ lab_1/tcp/t1/client.py
@@ -4,11 +4,17 @@
 import socket
 
 HOST = "127.0.0.1"	# The server's hostname or IP address
-PORT = 65432 		# The port used by the server
+PORT = 42069 		# The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
+	try:
+		s.connect((HOST, PORT))
+	except ConnectionRefusedError:
+		print('Unable to connect to server, is the server running?')
+		exit()
+
 	server_data = s.recv(1024)
 	s.sendall(b"I can hear you!")
+
 
 print(f"Received: {server_data.decode()}")
