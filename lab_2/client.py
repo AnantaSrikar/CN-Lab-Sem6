c956a475278a9349f76191d3d7b556ce04ddc522
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-03-13 12:01:35 +0530

    Basic UDP server ready - Data transmission works - TODO: Error detection algorithm with correction

--- lab_2/client.py
+++ lab_2/client.py
@@ -3,12 +3,21 @@
 
 import socket
 from random import randint
+from json import dumps
 
 # Simple function to flip a bit
 def flipBit(bit):
 	bit += 1
 	bit %= 2
 	return bit
+
+
+# Function to flip a random bit in the matrix
+def flipRandomMatrixBit(matrix, N):
+	randX = randint(0, N - 1)
+	randY = randint(0, N - 1)
+	matrix[randX][randY] = flipBit(matrix[randX][randY])
+	print(f"Flipped bit at ({randX}, {randY})")
 
 # Function to get a random matrix having 0s and 1s of size N
 def getRandomMatrix(N):
@@ -47,13 +56,30 @@ def getParity(matrix, N):
 		else:
 			matrix[i][N - 1] = parityJ
 
+# Function to send data to local server using UDP protocol
+def sendMatrix(matrix, N):
+	HOST = "127.0.0.1"				# The server's hostname or IP address
+	PORT = 42069 					# The port used by the server
+	serverAddrPort = (HOST, PORT)	# Convinience tuple
+
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+
+		# Send the random array
+		s.sendto(bytes(dumps({N: matrix}), 'utf-8'), serverAddrPort)
 
 N = 5
 
 matrix = getRandomMatrix(N - 1)
 getParity(matrix, N)
 
-for i in range(N):
-	print(matrix[i])
+# Randomly try to introduce an error
+if(randint(0, 1)):
+	flipRandomMatrixBit(matrix, N)
+
+print(dumps({N: matrix}))
+
+sendMatrix(matrix, N)
+
+# Now, we send this to the server using UDP protocol
 
 # Ideally one should be using __name__ == "__main__", but ehh lite
---
+++ lab_2/server.py
@@ -0,0 +1,42 @@
+# UDP server to recieve data and correct errors in it
+# Author: Srikar
+
+import socket
+from json import loads
+
+# Function to listen for clients
+def getDataFromClient():
+	HOST = "127.0.0.1"				# The server's hostname or IP address
+	PORT = 42069 					# The port used by the server
+	serverAddrPort = (HOST, PORT)	# Convinience tuple
+
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+		try:
+			s.bind(serverAddrPort)	# Claim the port
+
+		except OSError:
+			print("Port is busy! You're probably running the server already?")
+			exit()
+
+		while True:
+			try:
+				# Client should initiate a connection in UDP, due to the nature of the connectionless protocol
+				str_data = s.recvfrom(2048)
+				
+				client_addr = str_data[1]
+				str_data = str_data[0]
+				print(f"\nConnected to {client_addr}")
+
+				data_json = loads(str_data.decode())
+
+				print(data_json)
+
+			except KeyboardInterrupt:
+				print('Shutting down the server...')
+				break
+
+			# We should ideally never hit this, but nobody can predict the future ¯\_(ツ)_/¯
+			except Exception as e:
+				print(f"Something bad happened: {e}")
+
+getDataFromClient()
