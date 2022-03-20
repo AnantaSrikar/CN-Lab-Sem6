1e15625f04d0d38ca3fc260da737e4ebefe25fb9
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-03-20 09:56:36 +0530

    lab_3: init - reusing code from lab_2 as requested

---
+++ lab_3/client.py
@@ -0,0 +1,95 @@
+# UDP client that generates blocks of data along with row and column parity
+# Author: Srikar
+
+import socket
+from random import randint
+from json import dumps
+
+# Simple function to flip a bit
+def flipBit(bit):
+	bit += 1
+	bit %= 2
+	return bit
+
+# Function to flip a random bit in the matrix
+def flipRandomMatrixBit(matrix, N):
+	randX = randint(0, N - 1)
+	randY = randint(0, N - 1)
+	matrix[randX][randY] = flipBit(matrix[randX][randY])
+	print(f"Flipped bit at ({randX}, {randY})")
+
+# Function to get a random matrix having 0s and 1s of size N
+def getRandomMatrix(N):
+	matrix = []
+
+	for i in range(N):
+		matrix.append([])
+		for j in range(N):
+			matrix[i].append(randint(0, 1))
+
+	return matrix
+
+# Function to initialize parity
+def getParity(matrix, N):
+	
+	matrix.append([])
+	
+	# Initialzie column parities with 0
+	for i in range(N):
+		matrix[N - 1].append(0)
+
+	for i in range(N):
+		
+		parityJ = 0
+
+		for j in range(N - 1):
+			if(matrix[i][j]):
+
+				parityJ = flipBit(parityJ)
+
+				# Skip flipping column bits on last row
+				if(i < N - 1):
+					matrix[N - 1][j] = flipBit(matrix[N - 1][j])
+		
+		# For last row check
+		if(i != N - 1):
+			matrix[i].append(parityJ)
+		
+		else:
+			matrix[i][N - 1] = parityJ
+
+# Function to send data to local server using UDP protocol
+def sendMatrix(matrix):
+	HOST = "127.0.0.1"				# The server's hostname or IP address
+	PORT = 42069 					# The port used by the server
+	serverAddrPort = (HOST, PORT)	# Convinience tuple
+
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+
+		# Send the random array
+		s.sendto(bytes(dumps(matrix), 'utf-8'), serverAddrPort)
+
+if __name__ == "__main__":
+	
+	N = int(input('Enter N: '))
+
+	# Generating a random matrix of size N - 1
+	matrix = getRandomMatrix(N - 1)
+
+	# Assigning parity for the same
+	getParity(matrix, N)
+
+	for i in range(N):
+		print(matrix[i])
+
+	# Randomly try to introduce an error
+	if(randint(0, 1)):
+		flipRandomMatrixBit(matrix, N)
+
+	print("\n")
+
+	for i in range(N):
+		print(matrix[i])
+	
+	# Sending data to the server
+	sendMatrix(matrix)
---
+++ lab_3/server.py
@@ -0,0 +1,93 @@
+# UDP server to recieve data and correct errors in it
+# Author: Srikar
+
+import socket
+from json import loads
+
+from client import flipBit
+
+# Function to find bit error using parity bits
+def getFaults(matrix, N):
+	faultX = -1
+	faultY = -1
+
+	for i in range(N):
+		
+		parityX = 0
+		
+		for j in range(N - 1):
+			if(matrix[i][j]):
+				parityX = flipBit(parityX)
+
+		# Check if we found the error row
+		if (parityX != matrix[i][N - 1]):
+			faultX = i
+			break
+
+	for j in range(N):
+		
+		parityY = 0
+		
+		for i in range(N - 1):
+			if(matrix[i][j]):
+				parityY = flipBit(parityY)
+
+		# Check if we found the error row
+		if (parityY != matrix[N - 1][j]):
+			faultY = j
+			break
+
+	if(faultX * faultY >= 0 and faultX >= 0):
+		return (faultX, faultY)
+
+# Function to listen for clients
+def getDataFromClient():
+	HOST = "127.0.0.1"				# The server's hostname or IP address
+	PORT = 42069 					# The port used by the server
+	serverAddrPort = (HOST, PORT)	# Convinience tuple
+
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+		try:
+			s.bind(serverAddrPort)	# Claim the port
+
+		except OSError:
+			print("Port is busy! You're probably running the server already?")
+			exit()
+
+		try:
+			# Client should initiate a connection in UDP, due to the nature of the connectionless protocol
+			str_data = s.recvfrom(2048)
+			
+			client_addr = str_data[1]
+			str_data = str_data[0]
+			print(f"\nConnected to {client_addr}")
+
+			data_json = loads(str_data.decode())
+
+			return data_json
+
+		# We should ideally never hit this, but nobody can predict the future ¯\_(ツ)_/¯
+		except Exception as e:
+				print(f"Something bad happened: {e}")
+
+if __name__ == "__main__":
+
+	while(True):
+		try:
+			matrix = getDataFromClient()
+
+			N = len(matrix[0])
+			
+			faults = getFaults(matrix, N)
+
+			if(faults):
+				print(f"Bit flip detected at {faults}, correcting...")
+				matrix[faults[0]][faults[1]] = flipBit(matrix[faults[0]][faults[1]])
+
+			print("\nMatrix:")
+			for i in range(N):
+				print(matrix[i])
+
+		except KeyboardInterrupt:
+			print('Shutting down the server...')
+			break
