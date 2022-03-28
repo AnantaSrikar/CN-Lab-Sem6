493e3c76f202e9d4c9a96b2e611494180c00136e
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-03-28 12:18:52 +0530

    lab_4: List files in ftp server done - added template for functions

--- lab_4/client.py
+++ lab_4/client.py
@@ -1,23 +1,60 @@
 """
-    Basic FTP client
-    Author: Srikar
+	Basic FTP client
+	Author: Srikar
 """
 
 from ftplib import FTP
- 
+from os import system
+
+# Fixed server details
 HOST = "localhost"
 PORT = 42069
 
-ftp = FTP()
+# Function to list all files and directories on the server
+def ftpLS(ftp):
+	print("Files on the server: ")
+	
+	data = []
 
-ftp.connect(HOST, PORT)
-ftp.login()
- 
-data = []
- 
-ftp.dir(data.append)
- 
-ftp.quit()
- 
-for line in data:
-    print( "-", line)
+	ftp.dir(data.append)
+
+	for line in data:
+		print("-", line)
+
+def uploadToServer(ftp, local_file_path):
+	print(f"Upload {local_file_path} to server")
+
+def downloadFromServer(ftp, remote_file_path):
+	print(f"Download {remote_file_path} from server")
+
+if __name__ == "__main__":
+
+	ftp = FTP()
+
+	ftp.connect(HOST, PORT)
+	ftp.login()
+
+	while(True):
+		choice = input("\n1. See all files on server\n2. Upload file to server\n3. Download a file from server\nAnd anything else to exit!\n\nEnter choice: ")
+		choice = int(choice)
+
+		system('clear')
+
+		if choice == 1:
+			ftpLS(ftp)
+		
+		elif choice == 2:
+			# Probably have file autocompletion here. Will implement if I get enough sleep this week
+			local_file_path = input("Enter file path: ")
+			uploadToServer(ftp, local_file_path)
+
+		elif choice == 3:
+			# File path autocompletion possible here as well. But I need sleep
+			remote_file_path = input("Enter file path: ")
+			downloadFromServer(ftp, remote_file_path)
+
+		else:
+			print("Exitting...")
+			break
+
+	# ftp.quit()
